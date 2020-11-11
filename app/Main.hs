d17f39e6b8190781c4744026bafe716d3fad791e
Michael Lynch <lyncmi07@yahoo.co.uk>
2020-11-11 23:04:09 +0000

    Add parallel test function

--- app/Main.hs
+++ app/Main.hs
@@ -6,21 +6,21 @@ import Control.Parallel
 import Control.Concurrent
 
 main :: IO ()
-main = do
-    _ <- forkIO $ assertEquals "Equality test" 10 (10 + 10)
-    _ <- forkIO $ assertTrue "True test" (10 == (5 + 5))
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing iiiiitest" (10 == (takeAWhile 10000))
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    _ <- forkIO $ assertTrue "Failing test" False
-    return ()
+main =
+    testParallel [
+        assertEquals "Equality test" 10 (10 + 10),
+        assertTrue "True test" (10 == (5 + 5)),
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing iiiiitest" (10 == (takeAWhile 10000)),
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False,
+        assertTrue "Failing test" False]
 
 takeAWhile 0 = 100
 takeAWhile a = a + takeAWhile (a - 1)
--- src/Assert.hs
+++ src/Assert.hs
@@ -1,4 +1,6 @@
-module Assert where
+module Assert (assertTrue, assertEquals, testParallel) where
+
+import GHC.Conc
 
 assertEquals :: (Show a, Eq a) => String -> a -> a -> IO ()
 assertEquals description expected actual = do
@@ -12,4 +14,16 @@ assertTrue description value =
         then return ()
         else fail (description ++ " is not true")
 
+testParallel :: [IO ()] -> IO ()
+testParallel tests = do
+    threads <- sequence $ map forkIO tests
+    threadJoin threads
 
+threadJoin :: [ThreadId] -> IO ()
+threadJoin [] = return ()
+threadJoin (x:xs) = do
+    status <- threadStatus x
+    case status of
+        ThreadFinished -> threadJoin xs
+        ThreadDied -> fail "test thread failed"
+        otherwise -> threadJoin (x:xs)
